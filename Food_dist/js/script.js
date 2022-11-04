b1a07d4e45501153553c2f616103408aa7c734a5
Еркебулан Кундызбеков <erkebulankundyzbekov@MacBook-Air-Erkebulan.local>
2022-11-04 10:02:40 +0600

    timer implemtation and window parameters

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -1,36 +1,98 @@
-window.addEventListener('DOMContentLoaded',() => {
 
+window.addEventListener('DOMContentLoaded', function() {
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabContent = document.querySelectorAll('.tabcontent'),
-          tabParent = document.querySelector('.tabheader__items');
+    // Tabs
+    
+	let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
 
-    function hideTabContent() {
-        tabContent.forEach(element => {
-            element.classList.add('hide');
-            element.classList.remove('show', 'fade');
+	function hideTabContent() {
+        
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
-        tabs.forEach(element => {
-            element.classList.remove('tabheader__item_active');
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
         });
-    }
-    function showTab(i=0){
-        tabContent[i].classList.add('show', 'fade');
-        tabContent[i].classList.remove('hide');
+	}
+
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
+    
     hideTabContent();
-    showTab();
+    showTabContent();
 
-    tabParent.addEventListener('click', (event) => {
-        const target = event.target;
-        if(target && event.target.classList.contains('tabheader__item')){
-            tabs.forEach((item,i) => {
-                if(target == item){
+	tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
                     hideTabContent();
-                    showTab(i); 
+                    showTabContent(i);
                 }
             });
-        }
+		}
     });
+    
+    // Timer
+
+    const deadline = '2022-06-11';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
 });
---
+++ windowParameters/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;
+
+console.log(width, height);
+
+btn.addEventListener('click', () =>{
+    //box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect());
+
